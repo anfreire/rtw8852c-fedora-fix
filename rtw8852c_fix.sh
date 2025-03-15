9d6f658379755be3cbd76379523f4823527bba69
André Freire Ferreira <public@anfreire.dev>
2025-03-15 16:13:29 +0000

    fix: multiple macthes fix

--- rtw8852c_fix.sh
+++ rtw8852c_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Path to the known-working firmware version
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-WORKING_FW="$THIS_DIR/firmware/rtw8852c_fw.bin.xz"  # Local copy of working firmware
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+WORKING_FW="$THIS_DIR/firmware/rtw8852c_fw.bin.xz" # Local copy of working firmware
 
 # Check if working firmware exists
 if [ ! -e "$WORKING_FW" ]; then
@@ -11,10 +11,17 @@ if [ ! -e "$WORKING_FW" ]; then
 fi
 
 # Get the current firmware being used by the system
-current_fw=$(sudo dmesg | grep rtw8852c | grep -Po '[^\s]*.bin')
+current_fw=$(sudo dmesg | grep rtw8852c | grep -Po '([^\s]*.bin)' | head -n 1)
+
+# Check if the current firmware is the correct file ( == rtw89/rtw8852c_fw.bin)
+if [ "$current_fw" == "rtw89/rtw8852c_fw.bin" ]; then
+    echo "System is already using the working firmware. No changes needed."
+    exit 0
+fi
+
 current_fw_path="/lib/firmware/${current_fw}.xz"
 
-# CHeck if the current firmware exists
+# Check if the current firmware exists
 if [ ! -e "$current_fw_path" ]; then
     echo "Error: Current firmware not found at $current_fw_path"
     exit 1
